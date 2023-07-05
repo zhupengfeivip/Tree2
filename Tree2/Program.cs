2e7253a6ddced006a1dbb69928d543f3ac768857
zhupengfei <zhupengfeivip@163.com>
2023-07-05 15:31:34 +0800

    添加项目文件。

---
+++ Tree2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Tree2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            int depth = 3;
+            if (args.Length > 1)
+                depth = Convert.ToInt32(args[1]);
+            string[] dirs = System.IO.Directory.GetDirectories(path);
+            foreach (string dir in dirs)
+            {
+
+                string subdirectoryName = Path.GetFileName(dir);
+                Console.WriteLine(subdirectoryName);
+            }
+
+        }
+    }
+}
